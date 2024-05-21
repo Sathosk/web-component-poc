2498b30d7265891b3158632092f5f30304df30d0
Tiago P <tiago.pereira@fidelimax.com.br>
2024-05-21 12:05:48 -0300

    create utility functions to make code modular

--- web-component/src/scripts/componentLoader.ts
+++ web-component/src/scripts/componentLoader.ts
@@ -19,6 +19,41 @@ type TInstance = {
   console.log('Amazing plugin loaded');
   let instances: TInstance[] = [];
 
+  function createComponent( config: TConfig ) {
+    const component = document.createElement('amazing-component');
+
+    // Set props
+    component.setAttribute('name', config.props.name);
+    component.setAttribute('container-id', config.containerId);
+    component.setAttribute('id', config.containerId + '-target');
+
+    // Listen for the custom event
+    component.addEventListener('save', (event) => {
+      const customEvent = event as CustomEvent<string>;
+      config.onSave(customEvent.detail);
+    });
+
+    return component;
+  }
+
+  function addInstance(config: TConfig, component: HTMLElement) {
+    instances.push(
+      { 
+        containerId: config.containerId, 
+        component, 
+        onSave: config.onSave
+      }
+    );
+  }
+  
+  function removeInstance(containerId: string) {
+    instances = instances.filter(instance => instance.containerId !== containerId);
+  }
+
+  function findInstance(containerId: string) {
+    return instances.find(instance => instance.containerId === containerId);
+  }
+
   class AmazingComponentPlugin {
 
     createInstance(config: TConfig) {
@@ -30,27 +65,11 @@ type TInstance = {
 
       if (container) {
         console.log(`Creating instance on ${config.containerId} container`);
-        const component = document.createElement('amazing-component');
-
-        // Set props
-        component.setAttribute('name', config.props.name);
-        component.setAttribute('container-id', config.containerId);
-        component.setAttribute('id', config.containerId + '-target');
-
-        // Listen for the custom event
-        component.addEventListener('save', (event) => {
-          const customEvent = event as CustomEvent<string>;
-          config.onSave(customEvent.detail);
-        });
+        const component = createComponent(config);
 
         container.appendChild(component);
-        instances.push(
-          { 
-            containerId: config.containerId, 
-            component, 
-            onSave: config.onSave
-          }
-        );
+
+        addInstance(config, component)
         console.log('Instance created', instances);
       } else {
         console.error(`Container with id ${config.containerId} not found.`);
@@ -60,13 +79,15 @@ type TInstance = {
     deleteInstance(containerId: string) {
       if (containerId) {
         const container = document.getElementById(containerId);
-        const instance = instances.find(instance => instance.containerId === containerId);
+        // const instance = instances.find(instance => instance.containerId === containerId);
+        const instance = findInstance(containerId);
 
-        if (!instance) return console.error('Instance not found');
+        if (!instance) return console.error(`Instance with id ${containerId} not found`);
 
         console.log('Deleting instance');
         container?.removeChild(instance.component);
-        instances = instances.filter(instance => instance.containerId !== containerId);
+        // instances = instances.filter(instance => instance.containerId !== containerId);
+        removeInstance(containerId);
 
         console.log(`Instance ${containerId} deleted`);
       }
